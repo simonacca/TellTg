1f003c87a3ae3c31d67b1ba184d4e46d1c3d99cd
Simone Accascina <simon@accascina.me>
2015-10-08 14:37:48 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Tell Telegram',
-    version='1.0.0',
+    version='1.0.1',
     url='https://github.com/simonacca/TellTelegram',
     author='Simone Accascina',
     author_email='simon@accascina.me',
